0ce3b8dadd52b09253604c65a2121e71a4c163e0
Angel Peltekov <ai.peltekov@gmail.com>
2021-05-07 19:20:42 +0100

    Added a unit tests project

---
+++ FYP/TpmTest/UnitTest.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tpm2Lib;
+using TpmStorageHandler;
+
+namespace TpmTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static (TpmPublic pubKey, TpmHandle handle) GeneratePublicKey(Tpm2Wrapper tpm)
+        {
+            TpmHandle handle = tpm.CreateRsaPrimaryStorageKey(out TpmPublic pubKey);
+            return (pubKey, handle);
+        }
+
+        [TestMethod]
+        public void TestKeyCreation()
+        {
+            (TpmPublic pubKey, TpmHandle handle) result;
+            using (Tpm2Wrapper tpm = new Tpm2Wrapper())
+            {
+                AuthValue auth = AuthValue.FromRandom(8);
+                result = GeneratePublicKey(tpm);
+            }
+            Assert.IsNotNull(result.pubKey);
+        }
+    }
+}
