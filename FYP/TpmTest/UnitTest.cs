2f09942e6d5c79e3938cfd52d7c9b9aa1d13dc91
Angel Peltekov <ai.peltekov@gmail.com>
2021-05-16 00:42:35 +0100

    Tests for storage parent key, sealed data objects and encryption/decryption on the CPU using the unsealed data object

--- FYP/TpmTest/UnitTest.cs
+++ FYP/TpmTest/UnitTest.cs
@@ -6,10 +6,12 @@ using Tpm2Lib;
 using TpmStorageHandler;
 using Newtonsoft.Json;
 using System.IO;
+using System.Linq;
 using Windows.UI.Xaml;
 using Windows.Storage;
 using Windows.UI.WebUI;
 using TpmStorageHandler.Structures;
+using System.Security.Cryptography;
 
 namespace TpmTest
 {
@@ -62,12 +64,55 @@ namespace TpmTest
             return childKey;
         }
 
+        private static KeyWrapper LoadStorageParent(Tpm2Wrapper tpm, KeyWrapper primKey)
+        {
+            string storageParentJson = LoadFile("storageParent.sec");
+            Assert.IsNotNull(storageParentJson);
+            Assert.IsTrue(storageParentJson.Length > 0);
+
+            KeyWrapper storageParent = JsonConvert.DeserializeObject<KeyWrapper>(storageParentJson);
+            Assert.IsNotNull(storageParent);
+            Assert.IsNull(storageParent.Handle);
+
+            storageParent = tpm.LoadObject(
+                primKey.Handle, storageParent.KeyPriv, storageParent.KeyPub, null);
+            Assert.IsNotNull(storageParent.Handle);
+
+            return storageParent;
+        }
+
+        private static KeyWrapper LoadSealedObject(Tpm2Wrapper tpm, KeyWrapper storageParent,
+            out PolicySession policySession)
+        {
+            // Load sealed object
+            string sealedObjJson = LoadFile("sealedObj.sec");
+            Assert.IsNotNull(sealedObjJson);
+            Assert.IsTrue(sealedObjJson.Length > 0);
+
+            KeyWrapper sealedObj = JsonConvert.DeserializeObject<KeyWrapper>(sealedObjJson);
+            Assert.IsNotNull(sealedObj);
+            Assert.IsNotNull(sealedObj.KeyPriv);
+            Assert.IsNotNull(sealedObj.KeyPub);
+
+            // Object handle must be null as it is not loaded yet
+            Assert.IsNull(sealedObj.Handle);
+
+            // Load object
+            policySession = tpm.StartKeyedHashSession();
+            sealedObj = tpm.LoadObject(
+                storageParent.Handle, sealedObj.KeyPriv, sealedObj.KeyPub, null);
+            Assert.IsNotNull(sealedObj.Handle);
+
+            return sealedObj;
+        }
+
         private static void CleanUp(Tpm2Wrapper tpm, TpmHandle[] contexts)
         {
             foreach (TpmHandle context in contexts)
             {
                 tpm.FlushContext(context);
             }
+
             tpm.Dispose();
         }
 
@@ -76,7 +121,7 @@ namespace TpmTest
         {
             Tpm2Wrapper tpm = new Tpm2Wrapper();
             KeyWrapper result = tpm.GetPrimaryStorageKey(true);
-            
+
             Assert.IsNotNull(result.KeyPub);
         }
 
@@ -104,7 +149,7 @@ namespace TpmTest
 
             Assert.IsNotNull(encMessage);
 
-            CleanUp(tpm, new TpmHandle[] { primKey.Handle, childKey.Handle });
+            CleanUp(tpm, new TpmHandle[] {primKey.Handle, childKey.Handle});
         }
 
         [TestMethod]
@@ -122,7 +167,7 @@ namespace TpmTest
 
             Assert.AreEqual(expected, result);
 
-            CleanUp(tpm, new TpmHandle[] { childKey.Handle });
+            CleanUp(tpm, new TpmHandle[] {childKey.Handle});
         }
 
         [TestMethod]
@@ -162,17 +207,17 @@ namespace TpmTest
             // Store this in an object
             byte[] childPublic = storageParentDuplicate.Public.GetTpmRepresentation();
             byte[] childPrivate = storageParentDuplicate.Private.GetTpmRepresentation();
-            
+
             // Create a data structure containing all necessary data
             FileEncryptionData fed = new FileEncryptionData(
-                null, childPrivate, childPublic, dupeSession.PolicyHash, 
+                null, childPrivate, childPublic, dupeSession.PolicyHash,
                 storageParentDuplicate.EncKey, storageParentDuplicate.Seed, null);
             string fedJson = JsonConvert.SerializeObject(fed);
 
             // Save data on disk
             SaveFile(_dupekeyFileName, fedJson, out StorageFile _);
 
-            CleanUp(tpm, new TpmHandle[] { storageParentKey.Handle });
+            CleanUp(tpm, new TpmHandle[] {storageParentKey.Handle});
         }
 
         [TestMethod]
@@ -185,7 +230,7 @@ namespace TpmTest
 
             // File should not be empty
             Assert.IsNotNull(fed);
-            
+
             var childKey = ImportKey(tpm, fed, primKey);
 
             // Test to see if the key was loaded
@@ -229,7 +274,7 @@ namespace TpmTest
 
             //SaveFile(fedJson, out var file);
 
-            CleanUp(tpm, new TpmHandle[] { primKey.Handle, childKey.Handle });
+            CleanUp(tpm, new TpmHandle[] {primKey.Handle, childKey.Handle});
         }
 
         [TestMethod]
@@ -277,29 +322,135 @@ namespace TpmTest
         }
 
         [TestMethod]
-        public void TestCreateChildKeyFromStorageParent()
+        public void TestSealedObjectCreate()
         {
             // Set up
             Tpm2Wrapper tpm = new Tpm2Wrapper();
             KeyWrapper primKey = tpm.GetPrimaryStorageKey();
 
             // Load storage parent
-            string storageParentJson = LoadFile("storageParent.sec");
-            KeyWrapper storageParent = JsonConvert.DeserializeObject<KeyWrapper>(storageParentJson);
-            storageParent = tpm.LoadObject(
-                primKey.Handle, storageParent.KeyPriv, storageParent.KeyPub, null);
-            //TpmHandle storageParentHandle = tpm.LoadExternal(primKey, storageParent);
+            KeyWrapper storageParent = LoadStorageParent(tpm, primKey);
 
             // Create sealed object
-            KeyWrapper sealedObj = tpm.CreateSensitiveDataObject(primKey, null);
+            PolicySession policy = tpm.StartKeyedHashSession();
+            KeyWrapper sealedObj = tpm.CreateSensitiveDataObject(storageParent, policy);
 
             // Test if loaded
             Assert.IsNotNull(sealedObj.Handle);
 
             // Save to disk
-            SaveFile("sealedObj.enc", JsonConvert.SerializeObject(sealedObj), out StorageFile _);
+            SaveFile("sealedObj.sec", JsonConvert.SerializeObject(sealedObj), out StorageFile _);
 
-            CleanUp(tpm, new TpmHandle[] {/*storageParent.Handle, */sealedObj.Handle});
+            CleanUp(tpm, new TpmHandle[] {storageParent.Handle, sealedObj.Handle, policy.AuthSession});
+        }
+
+        [TestMethod]
+        public void TestSealedObjectLoad()
+        {
+            // Set up
+            Tpm2Wrapper tpm = new Tpm2Wrapper();
+            KeyWrapper primKey = tpm.GetPrimaryStorageKey();
+
+            // Load storage parent
+            KeyWrapper storageParent = LoadStorageParent(tpm, primKey);
+
+            // Load sealed data object
+            KeyWrapper sealedObj = LoadSealedObject(tpm, storageParent, out PolicySession policySession);
+
+            CleanUp(tpm, new TpmHandle[] {storageParent.Handle, sealedObj.Handle});
+        }
+
+        [TestMethod]
+        public void TestSealedObjectEncryptDecrypt()
+        {
+            // Set up
+            Tpm2Wrapper tpm = new Tpm2Wrapper();
+            KeyWrapper primKey = tpm.GetPrimaryStorageKey();
+
+            // Load storage parent
+            KeyWrapper storageParent = LoadStorageParent(tpm, primKey);
+
+            // Load sealed object
+            KeyWrapper sealedObj = LoadSealedObject(tpm, storageParent, out PolicySession policySession);
+
+            // Unseal
+            byte[] unsealedKey = tpm.UnsealObject(sealedObj, policySession);
+            Assert.IsNotNull(unsealedKey);
+            Assert.IsTrue(unsealedKey.Length > 0);
+
+            // Encrypt using .NET
+            AesCng aes = new AesCng
+            {
+                KeySize = 128,
+                BlockSize = 128,
+                Key = unsealedKey,
+                Mode = CipherMode.CBC
+            };
+            aes.GenerateIV();
+
+            byte[] bytesToEncrypt = {1, 2, 3};
+            ICryptoTransform encryptor = aes.CreateEncryptor(unsealedKey, aes.IV);
+            byte[] encrypted;
+            using (MemoryStream msEnc = new MemoryStream())
+            {
+                using (CryptoStream csEnc = new CryptoStream(msEnc, encryptor, CryptoStreamMode.Write))
+                {
+                    using (BinaryWriter bwEnc = new BinaryWriter(csEnc))
+                    {
+                        bwEnc.Write(bytesToEncrypt);
+                    }
+
+                    encrypted = msEnc.ToArray();
+                }
+            }
+
+            FileEncryptionData fed = new FileEncryptionData(
+                encrypted,
+                sealedObj.KeyPriv,
+                sealedObj.KeyPub,
+                null, null, null,
+                aes.IV);
+            const string fileName = "encryptedFileTest.enc";
+            SaveFile(fileName, JsonConvert.SerializeObject(fed), out StorageFile _);
+            fed = null;
+            aes.Dispose();
+
+            fed = LoadEncryptionDataFromFile(fileName);
+            AesCng aes2 = new AesCng()
+            {
+                KeySize = 128,
+                BlockSize = 128,
+                Key = unsealedKey,
+                Mode = CipherMode.CBC,
+                IV = fed.EncryptionIv
+            };
+
+            byte[] result;
+            using (var input = new MemoryStream(fed.FileData))
+            using (var output = new MemoryStream())
+            {
+                var decryptor = aes2.CreateDecryptor();
+                using (var cryptStream = new CryptoStream(input, decryptor, CryptoStreamMode.Read))
+                {
+                    var buffer = new byte[16];
+                    var read = cryptStream.Read(buffer, 0, buffer.Length);
+                    while (read > 0)
+                    {
+                        output.Write(buffer, 0, read);
+                        read = cryptStream.Read(buffer, 0, buffer.Length);
+                    }
+
+                    cryptStream.Flush();
+                    result = output.ToArray();
+                }
+            }
+
+            Assert.IsTrue(bytesToEncrypt.SequenceEqual(result));
+
+            aes2.Dispose();
+
+            CleanUp(tpm, new TpmHandle[] {storageParent.Handle, sealedObj.Handle});
+
         }
     }
 }
